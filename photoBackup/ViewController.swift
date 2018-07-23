36c4748e006397766b8c5b16ef572b97c1ab6b3e
张煜坤新西兰 <pine@pines-macbook-pro.local>
2018-07-23 18:37:34 +1200

    Initial Commit

---
+++ photoBackup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photoBackup
+//
+//  Created by Pine on 2018/7/23.
+//  Copyright © 2018年 Pine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
